504abd422d493df7cc10cd07b8ed98b06bedf2e3
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-12-03 15:12:19 +0200

    Add stdafx.h header

---
+++ AOCHeaders/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <math.h>
+
+#include "AOC.h"
