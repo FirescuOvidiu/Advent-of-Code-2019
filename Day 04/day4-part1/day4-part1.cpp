5555b964c307388abf455f74c948e6c8309aa808
Ovidiu Firescu <ovidiufirescu@gmail.com>
2019-12-04 09:30:33 +0200

    Renamed variables in Day 4

--- Day 04/day4-part1/day4-part1.cpp
+++ Day 04/day4-part1/day4-part1.cpp
@@ -6,29 +6,29 @@ int main()
 	std::fstream in("input.in", std::fstream::in);
 	std::fstream out("output.out", std::fstream::out);
 
-	bool prima = 0;
-	bool adoua = 1;
+	bool firstRule = false;
+	bool secondRule = true;
 	int aux = 0;
 	int nr = 0;
 	for (int i = 172930; i <= 683082; i++)
 	{
-		prima = 0;
-		adoua = 1;
+		firstRule = false;
+		secondRule = true;
 		aux = i;
 
 		while (i > 9)
 		{
 			if (i % 10 == i / 10 % 10)
 			{
-				prima = true;
+				firstRule = true;
 			}
 			if (i % 10 < i / 10 % 10)
 			{
-				adoua = false;
+				secondRule = false;
 			}
 			i = i / 10;
 		}
-		if (prima && adoua)
+		if (firstRule && secondRule)
 		{
 			nr++;
 		}
