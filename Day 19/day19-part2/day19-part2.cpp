45163985005e4480cb1db015ae1a6987458f7b42
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-12-19 08:02:13 +0200

    Add solution Day 19

---
+++ Day 19/day19-part2/day19-part2.cpp
@@ -0,0 +1,223 @@
+#include "../../AOCHeaders/stdafx.h"
+
+
+class Coordinate
+{
+public:
+	Coordinate(int x = 0, int y = 0) : x(x), y(y) {}
+
+	static int manhattanDistance(const Coordinate& c1, const Coordinate& c2)
+	{
+		return abs(c1.x - c2.x) + abs(c1.y - c2.y);
+	}
+
+	bool operator<(const Coordinate& c) const
+	{
+		return this->x < c.x ? true : this->x == c.x ? this->y < this->x : false;
+	}
+
+	bool operator==(const Coordinate& c) const
+	{
+		return ((this->x == c.x) && (this->y == c.y));
+	}
+
+public:
+	int x, y;
+};
+
+
+void readInput(std::fstream& in, std::vector<long long>& integers)
+{
+	long long number = 0;
+	char aux{};
+
+	while (in >> number)
+	{
+		integers.push_back(number);
+		in >> aux;
+	}
+}
+
+
+void setPosModes(std::vector<long long>& integers, long long currPos, long long relativeBase, long long& posMode1, long long& posMode2, long long& posMode3, long long opcode)
+{
+	if (currPos + 3 >= integers.size())
+	{
+		integers.resize(currPos + 4);
+	}
+
+	switch (integers[currPos] / 100 % 10)
+	{
+	case 0:
+		posMode1 = integers[currPos + 1];
+		break;
+
+	case 1:
+		posMode1 = currPos + 1;
+		break;
+
+	case 2:
+		posMode1 = relativeBase + integers[currPos + 1];
+		break;
+	}
+
+	switch (integers[currPos] / 1000 % 10)
+	{
+	case 0:
+		posMode2 = integers[currPos + 2];
+		break;
+
+	case 1:
+		posMode2 = currPos + 2;
+		break;
+
+	case 2:
+		posMode2 = relativeBase + integers[currPos + 2];
+		break;
+	}
+
+	switch (integers[currPos] / 10000 % 10)
+	{
+	case 0:
+		posMode3 = integers[currPos + 3];
+		break;
+
+	case 1:
+		posMode3 = currPos + 3;
+		break;
+
+	case 2:
+		posMode3 = relativeBase + integers[currPos + 3];
+		break;
+	}
+
+	if (posMode1 >= integers.size())
+	{
+		integers.resize(posMode1 + 1);
+	}
+
+	if (posMode2 >= integers.size() &&
+		((opcode == 1) || (opcode == 2) || (opcode == 5) || (opcode == 6) || (opcode == 7) || (opcode == 8)))
+	{
+		integers.resize(posMode2 + 1);
+	}
+
+	if ((posMode3 >= integers.size()) &&
+		((opcode == 1) || (opcode == 2) || (opcode == 7) || (opcode == 8)))
+	{
+		integers.resize(posMode3 + 1);
+	}
+}
+
+
+
+bool intCodeProgram(std::fstream& out, std::vector<long long> integers, long long input, const int x, const int y)
+{
+	long long currPos = 0;
+	long long posMode1 = 0;
+	long long posMode2 = 0;
+	long long posMode3 = 0;
+	long long relativeBase = 0;
+
+	while (integers[currPos] != 99)
+	{
+		setPosModes(integers, currPos, relativeBase, posMode1, posMode2, posMode3, integers[currPos] % 100);
+
+		switch (integers[currPos] % 100)
+		{
+		case 1:
+			integers[posMode3] = integers[posMode2] + integers[posMode1];
+			currPos = currPos + 4;
+			break;
+
+		case 2:
+			integers[posMode3] = integers[posMode2] * integers[posMode1];
+			currPos = currPos + 4;
+			break;
+
+		case 3:
+			input = input == -1 ? x : y;
+			integers[posMode1] = input;
+			currPos = currPos + 2;
+			break;
+
+		case 4:
+			return integers[posMode1];
+
+			out << integers[posMode1] << " ";
+			currPos = currPos + 2;
+			break;
+
+		case 5:
+			currPos = integers[posMode1] != 0 ? integers[posMode2] : currPos + 3;
+			break;
+
+		case 6:
+			currPos = integers[posMode1] == 0 ? integers[posMode2] : currPos + 3;
+			break;
+
+		case 7:
+			integers[posMode3] = integers[posMode1] < integers[posMode2];
+			currPos = currPos + 4;
+			break;
+
+		case 8:
+			integers[posMode3] = integers[posMode1] == integers[posMode2];
+			currPos = currPos + 4;
+			break;
+
+		case 9:
+			relativeBase += integers[posMode1];
+			currPos = currPos + 2;
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	std::fstream in("input.in", std::fstream::in);
+	std::fstream out("output.out", std::fstream::out);
+	std::vector<long long> integers;
+	long long input = -1;
+
+	readInput(in, integers);
+
+	int x = 5, y = 5;
+	bool meetCriterias = false;
+
+	while (!meetCriterias)
+	{
+		if (!intCodeProgram(out, integers, input, x, y))
+		{
+			x++;
+			while (!intCodeProgram(out, integers, input, x, y))
+			{
+				y++;
+			}
+			continue;
+		}
+
+		if (!intCodeProgram(out, integers, input, x, y + 99))
+		{
+			x++;
+			while (!intCodeProgram(out, integers, input, x, y))
+			{
+				y++;
+			}
+			continue;
+		}
+
+		if (!intCodeProgram(out, integers, input, x + 99, y))
+		{
+			y++;
+			continue;
+		}
+		out << x * 10000 + y;
+		meetCriterias = true;
+	}
+	
+	in.close();
+	out.close();
+}
