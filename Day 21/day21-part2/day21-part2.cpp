0da64a8274d0e3f833733533ccaad15d755d6bc2
FirescuOvidiu <ovidiufirescu@gmail.com>
2019-12-21 08:47:52 +0200

    Add solution Day 21

---
+++ Day 21/day21-part2/day21-part2.cpp
@@ -0,0 +1,191 @@
+#include "../../AOCHeaders/stdafx.h"
+
+
+void readInput(std::fstream& in, std::vector<long long>& integers)
+{
+	long long number = 0;
+	char aux{};
+
+	while (in >> number)
+	{
+		integers.push_back(number);
+		in >> aux;
+	}
+}
+
+
+void setPosModes(std::vector<long long>& integers, long long currPos, long long relativeBase, long long& posMode1, long long& posMode2, long long& posMode3, long long opcode)
+{
+	if (currPos + 3 >= integers.size())
+	{
+		integers.resize(currPos + 4);
+	}
+
+	switch (integers[currPos] / 100 % 10)
+	{
+	case 0:
+		posMode1 = integers[currPos + 1];
+		break;
+
+	case 1:
+		posMode1 = currPos + 1;
+		break;
+
+	case 2:
+		posMode1 = relativeBase + integers[currPos + 1];
+		break;
+	}
+
+	switch (integers[currPos] / 1000 % 10)
+	{
+	case 0:
+		posMode2 = integers[currPos + 2];
+		break;
+
+	case 1:
+		posMode2 = currPos + 2;
+		break;
+
+	case 2:
+		posMode2 = relativeBase + integers[currPos + 2];
+		break;
+	}
+
+	switch (integers[currPos] / 10000 % 10)
+	{
+	case 0:
+		posMode3 = integers[currPos + 3];
+		break;
+
+	case 1:
+		posMode3 = currPos + 3;
+		break;
+
+	case 2:
+		posMode3 = relativeBase + integers[currPos + 3];
+		break;
+	}
+
+	if (posMode1 >= integers.size())
+	{
+		integers.resize(posMode1 + 1);
+	}
+
+	if (posMode2 >= integers.size() &&
+		((opcode == 1) || (opcode == 2) || (opcode == 5) || (opcode == 6) || (opcode == 7) || (opcode == 8)))
+	{
+		integers.resize(posMode2 + 1);
+	}
+
+	if ((posMode3 >= integers.size()) &&
+		((opcode == 1) || (opcode == 2) || (opcode == 7) || (opcode == 8)))
+	{
+		integers.resize(posMode3 + 1);
+	}
+}
+
+
+void intCodeProgram(std::fstream& out, std::vector<long long>& integers, std::queue<long long>& input)
+{
+	long long currPos = 0;
+	long long posMode1 = 0;
+	long long posMode2 = 0;
+	long long posMode3 = 0;
+	long long relativeBase = 0;
+
+	while (integers[currPos] != 99)
+	{
+		setPosModes(integers, currPos, relativeBase, posMode1, posMode2, posMode3, integers[currPos] % 100);
+
+		switch (integers[currPos] % 100)
+		{
+		case 1:
+			integers[posMode3] = integers[posMode2] + integers[posMode1];
+			currPos = currPos + 4;
+			break;
+
+		case 2:
+			integers[posMode3] = integers[posMode2] * integers[posMode1];
+			currPos = currPos + 4;
+			break;
+
+		case 3:
+			integers[posMode1] = input.front();
+			input.pop();
+			currPos = currPos + 2;
+			break;
+
+		case 4:
+			if (integers[posMode1] > 127)
+			{
+				out << integers[posMode1] << " ";
+			}
+			currPos = currPos + 2;
+			break;
+
+		case 5:
+			currPos = integers[posMode1] != 0 ? integers[posMode2] : currPos + 3;
+			break;
+
+		case 6:
+			currPos = integers[posMode1] == 0 ? integers[posMode2] : currPos + 3;
+			break;
+
+		case 7:
+			integers[posMode3] = integers[posMode1] < integers[posMode2];
+			currPos = currPos + 4;
+			break;
+
+		case 8:
+			integers[posMode3] = integers[posMode1] == integers[posMode2];
+			currPos = currPos + 4;
+			break;
+
+		case 9:
+			relativeBase += integers[posMode1];
+			currPos = currPos + 2;
+			break;
+		}
+	}
+}
+
+
+void initQueue(const std::string& s, std::queue<long long>& input)
+{
+	for (int it = 0; it < s.size(); it++)
+	{
+		input.push((long long)s[it]);
+	}
+}
+
+
+int main()
+{
+	std::fstream in("input.in", std::fstream::in);
+	std::fstream out("output.out", std::fstream::out);
+	std::vector<long long> integers;
+	std::queue<long long> input;
+
+	// ((!A || !B || !C) && D) && (H || E)
+	// If I can jump again or walk forward one tile after jumping than jump
+	initQueue("NOT A T\n", input);
+	initQueue("OR T J\n", input);
+	initQueue("NOT B T\n", input);
+	initQueue("OR T J\n", input);
+	initQueue("NOT C T\n", input);
+	initQueue("OR T J\n", input);
+	initQueue("AND D J\n", input);
+
+	initQueue("NOT H T\n", input);
+	initQueue("NOT T T\n", input);
+	initQueue("OR E T\n", input);
+	initQueue("AND T J\n", input);
+
+	initQueue("RUN\n", input);
+
+	readInput(in, integers);
+	intCodeProgram(out, integers, input);
+
+	in.close();
+	out.close();
+}
